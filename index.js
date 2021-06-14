e0ebbc61638ddef757a156c5781fe66ee7988925
123qwe456 <aeman098.fatima@gmail.com>
2021-06-14 20:40:53 +0500

    ok

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 var cors = require('cors');
